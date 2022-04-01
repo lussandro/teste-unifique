6c47299db3cc14ad48b086f18569a4afd4e6f641
Lussandro <lussandro@gmail.com>
2022-04-01 06:16:14 -0300

    Create index.php

---
+++ html/index.php
@@ -0,0 +1,20 @@
+<html>
+<?php
+
+echo 'Versão Atual do PHP: ' . phpversion();
+
+$servername = "mysql-server";
+$username = "root";
+$password = "root";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Erro de Conexão: " . $conn->connect_error);
+}
+echo "<br /> Conectedo com Sucesso!";
+?>
+
+</html>
